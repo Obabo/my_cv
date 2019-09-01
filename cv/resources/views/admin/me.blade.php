fc47ac5b1b65405bdec09c819b68328aea540409
Dixon-obabo <cheche.oloo@gmail.com>
2019-09-01 09:17:57 +0300

    adding the admin page

---
+++ cv/resources/views/admin/me.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+this is the admin page to add more stuff and edit my app 
+  </body>
+</html>
